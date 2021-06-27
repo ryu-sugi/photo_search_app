f8460771b03203d395925ca42ed38b5de22c148c
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-06-27 20:19:38 +0900

    useStateの定義

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [text, setText] = useState('');
+  const [query, setQuery] = useState('apple');
+
   return (
     <div className="App">
       
